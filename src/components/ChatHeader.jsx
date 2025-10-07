a6e5bcb42d64e4e6bb377fd5430c67ba7e71520a
DenysSkachko <denya.skachko@gmail.com>
2025-10-08 02:37:54 +0300

    Updates UI in Sidebar, add fuctionallity to Chat Header online status

--- src/components/ChatHeader.jsx
+++ src/components/ChatHeader.jsx
@@ -1,22 +1,28 @@
 import { BsX } from 'react-icons/bs'
+import { useAuthStore } from '../store/useAuthStore'
 
 const ChatHeader = ({ selectedUser, setSelectedUser }) => {
+  const { onlineUsers } = useAuthStore()
   return (
     <div className="p-3 h-18 border-b border-dark text-lg font-semibold">
       <div className="flex items-center justify-between">
         <div className="flex items-center gap-3">
-          <div className="size-10 rounded-full overflow-hidden">
+          <div className="size-12 rounded-full overflow-hidden">
             <img src={selectedUser.profilePic || '/avatar.png'} alt={selectedUser.fullName} />
           </div>
 
-          <div className="flex items-center gap-4">
-            <h4 className="font-medium">{selectedUser.fullName}</h4>
-            <p className="text-sm text-green-500">online</p>
+          <div className="flex flex-col">
+            <h4 className="font-medium text-md text-white">{selectedUser.fullName}</h4>
+            {onlineUsers.includes(selectedUser._id) ? (
+              <span className=" text-green-400 text-xs">online</span>
+            ) : (
+              <span className="text-gray-500 text-xs">offline</span>
+            )}
           </div>
         </div>
 
         <button onClick={() => setSelectedUser(null)}>
-          <BsX />
+          <BsX className="size-8"/>
         </button>
       </div>
     </div>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -15,7 +15,7 @@ const Sidebar = () => {
   if (isUsersLoading) return 1
 
   return (
-    <aside className="h-full w-20 lg:w-72 border-r border-dark bg-accent text-dark flex flex-col transition-all duration-300">
+    <aside className="h-full w-20 lg:w-72 border-r border-dark bg-middle text-dark flex flex-col transition-all duration-300">
       <div className="border-b border-dark w-full p-5 h-18">
         <div className="flex items-center gap-4">
           <FaUser className="size-6" />
@@ -28,8 +28,8 @@ const Sidebar = () => {
           <button
             key={user._id}
             onClick={() => setSelectedUser(user)}
-            className={`w-full p-3 flex items-center gap-3 hover:bg-dark-alt ${
-              selectedUser?._id === user._id ? 'bg-middle' : ''
+            className={`w-full p-3 flex items-center gap-3 transition-all duration-300 hover:bg-dark-alt ${
+              selectedUser?._id === user._id ? 'bg-dark-alt' : ''
             }`}
           >
             <div className="relative mx-auto lg:mx-0 flex items-center gap-4">
@@ -42,12 +42,12 @@ const Sidebar = () => {
                 <span className="absolute bottom-0 right-0 size-3 bg-green-400 rounded-full"></span>
               )}
             </div>
-            <div className="hidden lg:flex">
-              <span className="font-medium "> {user.fullName}</span>
+            <div className="hidden lg:flex flex-col items-start">
+              <span className="font-medium text-md text-white"> {user.fullName}</span>
               {onlineUsers.includes(user._id) ? (
-                <span className=" text-green-400">Online</span>
+                <span className=" text-green-400 text-xs">online</span>
               ) : (
-                <span className="text-gray-500">Offline</span>
+                <span className="text-gray-500 text-xs">offline</span>
               )}
             </div>
           </button>
