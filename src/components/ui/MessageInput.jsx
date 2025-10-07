763e009cfd88e90b0b0aca1f7a566fe436861cc7
DenysSkachko <denya.skachko@gmail.com>
2025-10-07 03:31:30 +0300

    Add preview image and remove image

--- src/components/ui/MessageInput.jsx
+++ src/components/ui/MessageInput.jsx
@@ -8,15 +8,44 @@ const MessageInput = () => {
   const fileInputRef = useRef(null)
   const { sendMessage } = useChatStore()
 
-  const handleImageChange = e => {}
+  const handleImageChange = e => {
+    const file = e.target.files[0]
+    if (!file.type.startsWith('image/')) {
+      toast.error('Please select an image file')
+      return
+    }
 
-  const removeImage = () => {}
+    const reader = new FileReader()
+    reader.onloadend = () => {
+      setImagePreview(reader.result)
+    }
+    reader.readAsDataURL(file)
+  }
+
+  const removeImage = () => {
+    setImagePreview(null)
+    if (fileInputRef.current) fileInputRef.current.value = ''
+  }
 
   const handleSendMessage = async e => {}
 
   return (
     <div className="p-4 w-full">
-      
+      {imagePreview && (
+        <div className="mb-3 flex items-center gap-2">
+          <div className="relative">
+            <img src={imagePreview} alt="Preview" className="w-20 h-20 object-cover rounded-lg" />
+            <button
+              onClick={removeImage}
+              className="absolute -top-1.5 -right-1.5 w-5 h-5 rounded-full bg-middle
+              flex items-center justify-center"
+              type="button"
+            >
+              <BsX className="size-3" />
+            </button>
+          </div>
+        </div>
+      )}
       <form onSubmit={handleSendMessage} className="flex items-center gap-2">
         <div className="flex-1 flex gap-2 items-center">
           <input
